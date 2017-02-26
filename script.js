b8baa44311dfe1beba30d99775bb3884f689f75a
SartellSchoolDistrict <21rivajon@gapps.sartell.k12.mn.us>
2017-02-26 09:31:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+var timerValue = 60;
+var timerBool = false;
 function squareNumber(){
   var squared = document.mainform.firstinput.value;
   squared = squared * squared;
   document.getElementById("result").innerHTML = "Result: " + squared;
+}
+
+function switchTimerBool(){
+  if(timerBool === true){
+     timerBool = false;
+  }else if(timerBool === false){
+     timerBool = true;
+  }
+}
+
+function CheckTimer(){
+  document.mainform.timer
 }
