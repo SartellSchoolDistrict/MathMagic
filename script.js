9fb6d3b1aeae524f2f3f83a97a0f753fa1ce8677
SartellSchoolDistrict <amanda.holstrom@sartell.k12.mn.us>
2017-03-07 20:42:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var timerValue = 60;
 var timerBool = true;
 var doneQuestions = new Array(9);
 var toCheck = 0;
-bool keepQuestion = false;
+//bool keepQuestion = false;
 /*function  Start(){
   //Called on window open. Use this insted of space above to keep more organized
   var timerValue = 60;
@@ -64,14 +64,14 @@ function getRandomNumber(){
 }
 
 function startQuestions(){
-  keepQuestion = true;
+  //keepQuestion = true;
   displayQuestions();
 }
 
 function displayQuestions(){
-  if(keepQuestion){
+  //if(keepQuestion){
     toCheck = getRandomNumber();
     document.getElementById("Question").innerHTML = toCheck.toString();
-    keepQuestion = false;
-  }
+    //keepQuestion = false;
+  //}
 }
