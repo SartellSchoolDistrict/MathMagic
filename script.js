fbf5c75444b5d2eaaee37070b83731a650d27afa
SartellSchoolDistrict <amanda.holstrom@sartell.k12.mn.us>
2017-03-08 07:12:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -58,16 +58,16 @@ function startQuestions(){
 
 function displayQuestions(){
     toCheck = getRandomNumber();
- // document.getElementById("Question").innerHTML = toCheck.toString();
-  if(checkArray(doneQuestions[], toCheck)){
+    document.getElementById("Question").innerHTML = toCheck.toString();
+  /*if(checkArray(doneQuestions[], toCheck)){
     displayQuestions();
   }else{
     document.getElementById("Question").innerHTML = toCheck.toString();
-  }
+  }*/
   
 }
 
-function checkArray(a[], checkNum){
+/*function checkArray(a[], checkNum){
   for(i = 0; i < a.length; i++){
     if(a[i] === checkNum){
       return true;
@@ -77,4 +77,4 @@ function checkArray(a[], checkNum){
     }
     return false;
   }
-}
+}*/
